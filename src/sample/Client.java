d7a2cd28c99942e81761aea738153d18fc98cdf4
Märt Bakhoff <mart.bakhoff@gmail.com>
2019-01-28 00:42:57 +0200

    try-with-resources

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -7,14 +7,13 @@ public class Client {
 
   public static void main(String[] args) throws Exception {
     System.out.println("connecting to server");
-    Socket socket = new Socket("localhost", 8080);
-    System.out.println("connected; sending data");
-    OutputStream out = socket.getOutputStream();
-    int byteToSend = 42;
-    out.write(byteToSend);
-    System.out.println("sent " + byteToSend);
-    out.close();
-    socket.close();
-    System.out.println("cleaned up");
+    try (Socket socket = new Socket("localhost", 8080);
+         OutputStream out = socket.getOutputStream()) {
+      System.out.println("connected; sending data");
+      int byteToSend = 42;
+      out.write(byteToSend);
+      System.out.println("sent " + byteToSend);
+    }
+    System.out.println("finished");
   }
 }
--- src/sample/Server.java
+++ src/sample/Server.java
@@ -7,16 +7,15 @@ import java.net.Socket;
 public class Server {
 
   public static void main(String[] args) throws Exception {
-    ServerSocket ss = new ServerSocket(8080);
-    System.out.println("now listening on :8080");
-    Socket socket = ss.accept();
-    System.out.println("client connected; waiting for a byte");
-    InputStream in = socket.getInputStream();
-    int receivedByte = in.read();
-    System.out.println("received " + receivedByte);
-    in.close();
-    socket.close();
-    ss.close();
-    System.out.println("cleaned up");
+    try (ServerSocket ss = new ServerSocket(8080)) {
+      System.out.println("now listening on :8080");
+      try (Socket socket = ss.accept();
+           InputStream in = socket.getInputStream()) {
+        System.out.println("client connected; waiting for a byte");
+        int receivedByte = in.read();
+        System.out.println("received " + receivedByte);
+      }
+    }
+    System.out.println("finished");
   }
 }
